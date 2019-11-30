cdd063d9e17784575f9102d20dd3405a8496a478
Syahruddin <syahruddinr@gmail.com>
2019-12-01 05:34:30 +0700

    api selesai

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import flask
 import requests
+import json
 from flask import request, jsonify ,request
 
 app = flask.Flask(__name__)
@@ -9,26 +10,166 @@ app.config["DEBUG"] = True
 #masukin api key disini, ingatkan untuk ganti tiap 24 jam
 apikey = 'RGAPI-cec5aec1-54ed-4865-87ec-5870e601c44e'
 
+
+#home, kosong
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Yasapi</h1> <p>home<p>"
 
-@app.route('/match/matches', methods=['GET'])   
-def api_id():
+#untuk data2 umum di match tersebut  
+@app.route('/match/matches_info', methods=['GET'])   
+def matchinfo():
     if 'match_id' in request.args:
         match_id = int(request.args['match_id'])
     else:
         return "Error: No match field provided. Please specify an match."
 
     # Create an empty list for our results
-    results = []
+    matchdata = {
+      "gameDuration": 0, 
+      "gameId": 0, 
+      "gameMode": " ", 
+      "gameType": " ", 
+      "gameVersion": " ", 
+      "mapId": 0, 
+      "teamWin": 0,
+      "bans": []
+    }
     
     url = "https://na1.api.riotgames.com/lol/match/v4/matches/" + str(match_id) + "?api_key=" + apikey
     
     response = requests.get(url)
-    print(response)
-    response
+    
+    matchdata['gameDuration'] = response.json()['gameDuration']
+    matchdata['gameId'] = response.json()['gameId']
+    matchdata['gameMode'] = response.json()['gameMode']
+    matchdata['gameType'] = response.json()['gameType']
+    matchdata['gameVersion'] = response.json()['gameVersion']
+    matchdata['mapId'] = response.json()['mapId']
+    matchdata['bans'].extend(response.json()['teams'][0]['bans'])
+    matchdata['bans'].extend(response.json()['teams'][1]['bans'])
+    if response.json()['teams'][0]['win'] == 'Win':
+        matchdata['teamWin'] = 1
+    else:
+        if response.json()['teams'][1]['win'] == 'Win':
+            matchdata['teamWin'] = 2
+
+
+    
+    return matchdata
+
+
+#untuk info pemain yg terlibat seperti champion yg dipakai, jumlah k/d/a, dll    
+@app.route('/match/participant', methods=['GET'])   
+def party():
+    if 'match_id' in request.args:
+        match_id = int(request.args['match_id'])
+    else:
+        return "Error: No match field provided. Please specify an match."
+    
+    
+    participant = [
+                    {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                     {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                     {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                     {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                     {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                     {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                     {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                     {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                     {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                     {
+                        "summonerName": " ",
+                        "championId": 0, 
+                        "kills": 0, 
+                        "deaths": 0, 
+                        "assists": 0
+                    },
+                ]
+    
+    url = "https://na1.api.riotgames.com/lol/match/v4/matches/" + str(match_id) + "?api_key=" + apikey
+    
+    response = requests.get(url)
+    iteratif = 0
+    while iteratif < len(participant):
+        participant[iteratif]['summonerName'] = response.json()['participantIdentities'][iteratif]['player']['summonerName']
+        participant[iteratif]['championId'] = response.json()['participants'][iteratif]['championId']
+        participant[iteratif]['kills'] = response.json()['participants'][iteratif]['stats']['kills']
+        participant[iteratif]['deaths'] = response.json()['participants'][iteratif]['stats']['deaths']
+        participant[iteratif]['assists'] = response.json()['participants'][iteratif]['stats']['assists']
+        iteratif+=1
+    
+    return jsonify(participant)
+    
+    
+
+@app.route('/match/matches', methods=['GET'])   
+def match():
+    if 'match_id' in request.args:
+        match_id = int(request.args['match_id'])
+    else:
+        return "Error: No match field provided. Please specify an match."
+
+    
+    url = "https://na1.api.riotgames.com/lol/match/v4/matches/" + str(match_id) + "?api_key=" + apikey
+    
+    response = requests.get(url)
     
     return response.json()
+    
 
 app.run()
