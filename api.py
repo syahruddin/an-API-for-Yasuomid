ef620a6a1089fc94a72b014acff7e9f3bf91e98a
Syahruddin <syahruddinr@gmail.com>
2019-12-01 03:01:36 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,29 +1,34 @@
 import flask
-from flask import request, jsonify
+import requests
+from flask import request, jsonify ,request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-apikey = ''
+
+#masukin api key disini, ingatkan untuk ganti tiap 24 jam
+apikey = 'RGAPI-cec5aec1-54ed-4865-87ec-5870e601c44e'
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Yasapi</h1> <p>home<p>"
 
-@app.route('/lol/match/v4/matches', methods=['GET'])   
+@app.route('/match/matches', methods=['GET'])   
 def api_id():
-    if 'username' in request.args:
-        username = string(request.args['username'])
-        if 'region' in request.args:
-            region = string(request.args['region'])
-        else:
-            return "Error: No region field provided. Please specify an region."
+    if 'match_id' in request.args:
+        match_id = int(request.args['match_id'])
     else:
-        return "Error: No username field provided. Please specify an username."
+        return "Error: No match field provided. Please specify an match."
 
     # Create an empty list for our results
-    results = username + ' ' + region
+    results = []
     
-    return jsonify(results)
+    url = "https://na1.api.riotgames.com/lol/match/v4/matches/" + str(match_id) + "?api_key=" + apikey
+    
+    response = requests.get(url)
+    print(response)
+    response
+    
+    return response.json()
 
 app.run()
