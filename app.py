80dbbd7b5224faf1bfaac58da76d8789228e4088
Syahruddin <syahruddinr@gmail.com>
2019-12-01 11:52:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -170,5 +170,6 @@ def match():
     
     return response.json()
     
-
-app.run()
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
