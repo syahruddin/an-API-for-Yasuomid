63decb9cbcd95fcb29d4cd60a8651a24a46bedae
Syahruddin <syahruddinr@gmail.com>
2019-12-05 20:32:19 +0700

    reset key

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 #masukin api key disini, ingatkan untuk ganti tiap 24 jam
-apikey = 'RGAPI-b642a59a-81b7-48df-80ac-c998b750330f'
+apikey = 'RGAPI-8b198002-537c-4eec-ac2b-09fe41e7e40e'
 
 
 #home, kosong
